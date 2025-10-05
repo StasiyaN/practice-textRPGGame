8095a965f93bef3aebcfd18f0a71aa763c26f88a
StasiyaN <ana.nyzhalska@gmail.com>
2025-10-05 12:13:45 +0200

    started writing js, removed html where info will be added dynamically

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Happy developing ✨')
+//Создаем объекты игры
+const game = {
+    player: null,
+    currentLocation: null,
+    currentEnemy: null
+    locations: {},
+    enemies: {},
+    items: {},
+    logEntries: [],
+    isInCombat: false,
+
+    init: function() {
+        this.createItems();
+        this.createEnemies();
+        this.createLocations();
+        this.createPlayer();
+
+        this.currentLocation = this.location.start;
+        this.updateUI();
+        this.addLogEntry("Добро пожаловать в мир приключений! Вы стоите у входа в таинственный лес.", "location");
+    },
+
+    createPlayer: function() {
+        this.player = game.player;
+    }
+}
